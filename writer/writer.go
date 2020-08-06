c6b667df555b86cfe96c5ce17875c2bc4f7fce6c
Dominik Braun <mail@dominikbraun.io>
2020-08-06 23:15:13 +0200

    Implement build.Writer interface

--- writer/writer.go
+++ writer/writer.go
@@ -1,3 +1,16 @@
 package writer
 
+import (
+	"github.com/verless/verless/model"
+)
+
+func New() *writer {
+	w := writer{}
+	return &w
+}
+
 type writer struct{}
+
+func (w *writer) Write(site model.Site) error {
+	return nil
+}
