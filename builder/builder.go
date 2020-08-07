cbe59b5baf8296412b5b7b17b362da4d3452a4bd
Dominik Braun <mail@dominikbraun.io>
2020-08-07 14:07:28 +0200

    Bind route walking functions to model.Site type

--- builder/builder.go
+++ builder/builder.go
@@ -1,13 +1,9 @@
 package builder
 
 import (
-	"fmt"
 	"github.com/verless/verless/model"
-	"strings"
 	"sync"
 )
-
-type walkFn func(route *model.Route) error
 
 func New() *builder {
 	b := builder{
@@ -26,7 +22,7 @@ func (b *builder) RegisterPage(route string, page model.Page) error {
 	b.mutex.Lock()
 	defer b.mutex.Unlock()
 
-	r := b.createRoute(route)
+	r := b.site.CreateRoute(route)
 	r.Pages = append(r.Pages, page)
 
 	return nil
@@ -34,69 +30,4 @@ func (b *builder) RegisterPage(route string, page model.Page) error {
 
 func (b *builder) Dispatch() (model.Site, error) {
 	return b.site, nil
-}
-
-func (b *builder) walkRoutes(walk walkFn, maxDepth int) error {
-	return b.walkRoute(&b.site.Root, walk, maxDepth, 0)
-}
-
-func (b *builder) walkRoute(route *model.Route, walk walkFn, maxDepth, curDepth int) error {
-	if maxDepth != -1 && curDepth == maxDepth {
-		return nil
-	}
-	curDepth++
-
-	if err := walk(route); err != nil {
-		return err
-	}
-
-	for _, child := range route.Children {
-		if err := b.walkRoute(child, walk, maxDepth, curDepth); err != nil {
-			return err
-		}
-	}
-
-	return nil
-}
-
-func (b *builder) createRoute(route string) *model.Route {
-	var (
-		node     = &b.site.Root
-		segments = strings.Split(route, "/")
-	)
-
-	for i, s := range segments {
-		if _, exists := node.Children[s]; !exists {
-			node.Children[s] = &model.Route{
-				Children:  make(map[string]*model.Route),
-				Pages:     make([]model.Page, 0),
-				IndexPage: model.IndexPage{},
-			}
-		}
-		if i == len(segments)-1 {
-			return node
-		}
-		node = node.Children[s]
-	}
-
-	return nil
-}
-
-func (b *builder) resolveRoute(route string) (*model.Route, error) {
-	var (
-		node     = &b.site.Root
-		segments = strings.Split(route, "/")
-	)
-
-	for i, s := range segments {
-		if i == len(segments)-1 {
-			return node, nil
-		}
-		if _, exists := node.Children[s]; !exists {
-			return node, fmt.Errorf("child route %s does not exist", s)
-		}
-		node = node.Children[s]
-	}
-
-	return node, fmt.Errorf("route %s does not exist", route)
 }
--- model/site.go
+++ model/site.go
@@ -1,8 +1,80 @@
 package model
 
+import (
+	"fmt"
+	"strings"
+)
+
+type walkFn func(path string, route *Route) error
+
 type Site struct {
 	Meta   Meta
 	Nav    Nav
 	Root   Route
 	Footer Footer
+}
+
+func (s *Site) WalkRoutes(walkFn walkFn, maxDepth int) error {
+	return s.walkRoute("", &s.Root, walkFn, maxDepth, 0)
+}
+
+func (s *Site) walkRoute(path string, route *Route, walkFn walkFn, maxDepth, curDepth int) error {
+	if maxDepth != -1 && curDepth == maxDepth {
+		return nil
+	}
+	curDepth++
+
+	if err := walkFn(path, route); err != nil {
+		return err
+	}
+
+	for path, child := range route.Children {
+		if err := s.walkRoute(path, child, walkFn, maxDepth, curDepth); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (s *Site) CreateRoute(route string) *Route {
+	var (
+		node     = &s.Root
+		segments = strings.Split(route, "/")
+	)
+
+	for i, s := range segments {
+		if _, exists := node.Children[s]; !exists {
+			node.Children[s] = &Route{
+				Children:  make(map[string]*Route),
+				Pages:     make([]Page, 0),
+				IndexPage: IndexPage{},
+			}
+		}
+		if i == len(segments)-1 {
+			return node
+		}
+		node = node.Children[s]
+	}
+
+	return nil
+}
+
+func (s *Site) ResolveRoute(route string) (*Route, error) {
+	var (
+		node     = &s.Root
+		segments = strings.Split(route, "/")
+	)
+
+	for i, s := range segments {
+		if i == len(segments)-1 {
+			return node, nil
+		}
+		if _, exists := node.Children[s]; !exists {
+			return node, fmt.Errorf("child route %s does not exist", s)
+		}
+		node = node.Children[s]
+	}
+
+	return node, fmt.Errorf("route %s does not exist", route)
 }
