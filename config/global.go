89f70914e74983f923ea0a207f2b8d186380111f
Dominik Braun <mail@dominikbraun.io>
2020-08-07 15:12:25 +0200

    Implement basic writer functionality

---
+++ config/global.go
@@ -0,0 +1,11 @@
+package config
+
+const (
+	ContentDir   string = "content"
+	TemplateDir  string = "templates"
+	AssetDir     string = "assets"
+	OutputDir    string = "target"
+	IndexFile    string = "index.html"
+	PageTpl      string = "page.html"
+	IndexPageTpl string = "index-page.html"
+)
--- writer/writer.go
+++ writer/writer.go
@@ -1,25 +1,95 @@
 package writer
 
 import (
+	"github.com/verless/verless/config"
 	"github.com/verless/verless/model"
 	"os"
 	"path/filepath"
+	"text/template"
 )
 
-func New(path, outputDir string) *writer {
+func New(path, outputDir string) (*writer, error) {
+	outputPath := filepath.Join(path, outputDir)
+
 	w := writer{
-		path:      path,
-		outputDir: outputDir,
+		path:       path,
+		outputPath: outputPath,
 	}
-	return &w
+
+	if err := w.initTemplates(); err != nil {
+		return nil, err
+	}
+
+	return &w, nil
 }
 
 type writer struct {
-	path      string
-	outputDir string
+	path         string
+	outputPath   string
+	site         model.Site
+	pageTpl      *template.Template
+	indexPageTpl *template.Template
 }
 
 func (w *writer) Write(site model.Site) error {
-	_ = os.Mkdir(filepath.Join(w.path, w.outputDir), 0700)
+	w.site = site
+
+	err := w.site.WalkRoutes(func(path string, route *model.Route) error {
+		for _, page := range route.Pages {
+			if err := w.writePage(path, &page); err != nil {
+				return err
+			}
+		}
+		return w.writeIndexPage(path, &route.IndexPage)
+	}, -1)
+
+	return err
+}
+
+func (w *writer) initTemplates() error {
+	var (
+		err          error
+		pageTpl      = filepath.Join(w.path, config.TemplateDir, config.PageTpl)
+		indexPageTpl = filepath.Join(w.path, config.TemplateDir, config.IndexPageTpl)
+	)
+
+	if w.pageTpl, err = template.ParseFiles(pageTpl); err != nil {
+		return err
+	}
+
+	if w.indexPageTpl, err = template.ParseFiles(indexPageTpl); err != nil {
+		return err
+	}
+
 	return nil
+}
+
+func (w *writer) writePage(route string, page *model.Page) error {
+	path := filepath.Join(w.outputPath, route, page.ID)
+
+	if err := os.MkdirAll(path, 0700); err != nil {
+		return err
+	}
+
+	file, err := os.Create(filepath.Join(path, config.IndexFile))
+	if err != nil {
+		return err
+	}
+
+	return w.pageTpl.Execute(file, &page)
+}
+
+func (w *writer) writeIndexPage(route string, indexPage *model.IndexPage) error {
+	path := filepath.Join(w.outputPath, route)
+
+	if err := os.MkdirAll(path, 0700); err != nil {
+		return err
+	}
+
+	file, err := os.Create(filepath.Join(path, config.IndexFile))
+	if err != nil {
+		return err
+	}
+
+	return w.indexPageTpl.Execute(file, &indexPage)
 }
