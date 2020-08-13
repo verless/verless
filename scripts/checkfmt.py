2733972c847a5025a76aac6d82443c5f743cf135
Dominik Braun <mail@dominikbraun.io>
2020-08-13 13:57:18 +0200

    Create script for checking Go format for all files

---
+++ scripts/checkfmt.py
@@ -0,0 +1,23 @@
+import subprocess
+import sys
+
+
+def check_fmt():
+    """Run `gofmt -l ..` to find all Go files that aren't formatted
+    appropriately.
+
+    If all files are formatted appropriately, exit with status 0,
+    otherwise exit with status 1 by printing all invalid files.
+    """
+    process = subprocess.run(["gofmt", "-l", ".."], capture_output=True)
+    output = process.stdout.decode("utf-8")
+
+    if output != "":
+        msg = """gofmt found non-formatted code in the following files:
+{0}
+Run `go fmt ./...` to format your files and commit them again.""".format(output)
+
+        sys.exit(msg)
+
+
+check_fmt()
