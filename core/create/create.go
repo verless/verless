60bb2fbddae7907a7e7bc602ee072678632d93eb
Dominik Braun <mail@dominikbraun.io>
2020-08-18 10:53:16 +0200

    Remove unused constant

--- core/create/create.go
+++ core/create/create.go
@@ -6,29 +6,6 @@ import (
 	"path/filepath"
 )
 
-const (
-	// defaultConfig is the default YAML project configuration when
-	// a new project is initialized.
-	defaultConfig string = `# verless.yml is your project configuration.
-# Check out the example project for a full configuration:
-# https://github.com/verless/verless/tree/docs/example
-site:
-  # General information about your project.
-  meta:
-    title: 
-    subtitle: 
-    description: 
-    author: 
-    base: 
-# Enable plugins for your project.
-plugins:
-  - atom
-build:
-  # Allow verless to overwrite the output directory of your website
-  # when re-building it. False by default.
-  overwrite: false`
-)
-
 // Project creates a new verless project which is an exact copy
 // of the example project.
 func Project(path string) error {
