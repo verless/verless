94e1889c127a7d9ecff76fd5d0d9945f6fc0e4df
Dominik Braun <mail@dominikbraun.io>
2020-08-19 13:40:29 +0200

    Exit with error message

--- scripts/check_imports.py
+++ scripts/check_imports.py
@@ -1,8 +1,14 @@
 import subprocess
+import sys
 
 
 def check_imports():
-    output = subprocess.check_output(["goimports"])
+    output = subprocess.check_output(["goimports", "-l", ".."])
     output = output.decode("utf-8")
 
-    print(output)
+    if output != "":
+        msg = """goimports found non-ordered imports in the following files:
+{0}
+Run `goimports -w .` to order the imports or group and order them yourself.""".format(output)
+
+        sys.exit(msg)
