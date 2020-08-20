47868aeb076bddb68fae15dff94c09eafc3c98b6
Dominik Braun <mail@dominikbraun.io>
2020-08-20 15:08:08 +0200

    Add comments for goimports script

--- scripts/check_imports.py
+++ scripts/check_imports.py
@@ -3,6 +3,26 @@ import sys
 
 
 def check_imports():
+    """
+    Run `goimports -l ..` to find all Go files whose imports aren't
+    formatted appropriately.
+
+    Imported Go packages don't only have to be ordered alphabetical,
+    they also need to be grouped by imports from the standard library
+    and from third-party vendors. For example:
+
+    import (
+        "errors"
+        "os"
+
+        "github.com/verless/verless/builder"
+        "github.com/verless/verless/core/build"
+        "github.com/verless/verless/parser"
+    )
+
+    If all files are formatted appropriately, exit with status 0,
+    otherwise exit with status 1 by printing all invalid files.
+    """
     output = subprocess.check_output(["goimports", "-l", ".."])
     output = output.decode("utf-8")
 
