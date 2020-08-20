64cec93df6931b0ef0c385f0ca15bd7d4224bf3e
Dominik Braun <mail@dominikbraun.io>
2020-08-20 22:18:21 +0200

    Implement tpl package (#42)

---
+++ tpl/tpl.go
@@ -0,0 +1,43 @@
+package tpl
+
+import (
+	"errors"
+	"fmt"
+	"text/template"
+)
+
+var (
+	templates            map[string]*template.Template
+	ErrAlreadyRegistered = errors.New("template has already been registered")
+)
+
+func Register(key string, path string) (*template.Template, error) {
+	if templates == nil {
+		templates = make(map[string]*template.Template)
+	}
+
+	if _, exists := templates[key]; exists {
+		return nil, ErrAlreadyRegistered
+	}
+
+	tpl, err := template.ParseFiles(path)
+	if err != nil {
+		return nil, err
+	}
+
+	templates[key] = tpl
+
+	return templates[key], nil
+}
+
+func Get(key string) (*template.Template, error) {
+	if templates == nil {
+		templates = make(map[string]*template.Template)
+	}
+
+	if _, exists := templates[key]; !exists {
+		return nil, fmt.Errorf("template %s has not been registered", key)
+	}
+
+	return templates[key], nil
+}
