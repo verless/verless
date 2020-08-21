f92895387b1c0c0fb6704fde37d47ea4338f5ddc
Dominik Braun <mail@dominikbraun.io>
2020-08-21 16:36:20 +0200

    Change build path

--- scripts/build.py
+++ scripts/build.py
@@ -36,7 +36,11 @@ def build(os, arch):
     binary = "verless.exe" if os == "windows" else "verless"
     target = "target/{0}-{1}/{2}".format(os, arch, binary)
 
-    subprocess.Popen(["go", "build", "-v", "-o", target, "cmd/verless/main.go"], env={"GOOS": os, "GOARCH": arch})
+    env = os.environ.copy()
+    env["GOOS"] = os
+    env["GOARCH"] = arch
+
+    subprocess.Popen(["go", "build", "-v", "-o", target, "cmd/verless/main.go"], env=env)
 
 
 def package(os, arch):
