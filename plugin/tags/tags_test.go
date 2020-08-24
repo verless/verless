80e2dee907936737dd4eb622433547d0e1f9da38
Dominik Braun <mail@dominikbraun.io>
2020-08-24 12:44:37 +0200

    Create test for ProcessPage

---
+++ plugin/tags/tags_test.go
@@ -0,0 +1,52 @@
+package tags
+
+import (
+	"fmt"
+	"github.com/verless/verless/model"
+	"testing"
+)
+
+var (
+	tg    *tags        = nil
+	pages []model.Page = []model.Page{
+		{ID: "page-0", Tags: []string{"t-1", "t-2"}},
+		{ID: "page-1", Tags: []string{"t-1", "t-3"}},
+		{ID: "page-2", Tags: []string{"t-2", "t-3"}},
+		{ID: "page-3", Tags: []string{"t-2"}},
+	}
+)
+
+func TestTags_ProcessPage(t *testing.T) {
+	setupTags()
+
+	for i, page := range pages {
+		page.Route = getRoute(i)
+		if err := tg.ProcessPage(&page); err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	for _, page := range pages {
+		for _, tag := range page.Tags {
+			if _, exists := tg.m[tag]; !exists {
+				t.Fatalf("key for tag %s does not exist", tag)
+			}
+			if tg.m[tag] == nil {
+				t.Fatalf("IndexPage for tag %s is nil", tag)
+			}
+			if len(tg.m[tag].Pages) < 1 {
+				t.Errorf("expected at least %d pages for tag %s, got %d", 1, tag, 0)
+			}
+		}
+	}
+}
+
+func setupTags() {
+	if tg == nil {
+		tg = New("", "")
+	}
+}
+
+func getRoute(n int) string {
+	return fmt.Sprintf("/route-%v", n)
+}
