3c8e52a96f145986092c96ef9a91fdca32f492ce
aligator <aligator@suncraft-server.de>
2020-09-03 18:04:10 +0200

    add TestTags_PreWrite

--- plugin/tags/tags_test.go
+++ plugin/tags/tags_test.go
@@ -43,41 +43,50 @@ func TestTags_ProcessPage(t *testing.T) {
 				taggerTag, exists := tagger.m[tag]
 
 				test.Assert(t, exists, "tag should exist")
-				test.Equals(t, nil, taggerTag)
+				test.NotEquals(t, nil, taggerTag)
 				test.Assert(t, len(tagger.m[tag].Pages) > 0, "tag should exist")
 			}
 		}
 	}
 }
 
-/*
 func TestTags_PreWrite(t *testing.T) {
-	setupTags()
+	tests := map[string]struct {
+		tagsIndexPages map[string]*model.IndexPage
+		expectedError  error
+	}{
+		"normal index pages": {
+			tagsIndexPages: map[string]*model.IndexPage{
+				"test1": {},
+				"test2": {},
+				"test3": {},
+			},
+		},
+	}
 
-	for i, page := range pages {
-		page.Route = getRoute(i)
-		if err := tg.ProcessPage(&page); err != nil {
-			t.Fatal(err)
+	for name, testCase := range tests {
+		t.Log(name)
+
+		tagger := New()
+		tagger.m = testCase.tagsIndexPages
+		s := model.Site{}
+		err := tagger.PreWrite(&s)
+		if test.ExpectedError(t, testCase.expectedError, err) != test.IsCorrectNil {
+			continue
+		}
+
+		tags, ok := s.Root.Children["tags"]
+		test.Equals(t, true, ok)
+		test.NotEquals(t, nil, tags)
+
+		for tag := range tagger.m {
+			child, ok := tags.Children[tag]
+			test.Equals(t, true, ok)
+			test.NotEquals(t, nil, child)
+			test.NotEquals(t, nil, child.IndexPage)
+			test.NotEquals(t, nil, child.IndexPage.Page)
 		}
 	}
-
-	site := model.Site{}
-
-	if err := tg.PreWrite(&site); err != nil {
-		t.Fatal(err)
-	}
-
-	for _, page := range pages {
-		for _, tag := range page.Tags {
-			node, err := site.ResolveNode(filepath.Join(tagsDir, tag))
-			if err != nil {
-				t.Fatal(err)
-			}
-			if len(node.IndexPage.Pages) < 1 {
-				t.Errorf("expected at least %d pages for tag %s, got %d", 1, tag, 0)
-			}
-		}
-	}
-}*/
+}
 
 func TestTags_PostWrite(t *testing.T) {}
