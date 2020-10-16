bb8b6fb1fad9249fd471f2a98165f1d928bd7cd7
rehanvipin <rehanvipin@outlook.com>
2020-10-16 17:37:57 +0530

    Refactor fs_test.go
    Include more tests for old functions
    
    Add test function for CopyFromOS

--- fs/fs_test.go
+++ fs/fs_test.go
@@ -14,16 +14,21 @@ func TestIsSafeToRemove(t *testing.T) {
 		force    bool
 	}
 
+	// Create new in-memory FS with one file and one directory
 	tempFS := afero.NewMemMapFs()
+	_, _ = tempFS.Create("file.go")
+	_ = tempFS.Mkdir("directory", 0755)
+
 	tests := []struct {
 		name string
 		args args
 		want bool
 	}{
 		// Add test entries here
-		{"Remove Example", args{tempFS, "../example", false}, true},
-		{"Should not remove", args{tempFS, ".", false}, false},
-		{"Force Remove", args{tempFS, ".", true}, true},
+		{"Remove file", args{tempFS, "file.go", false}, false},
+		{"Remove directory", args{tempFS, "directory", false}, false},
+		{"Remove non-existent file", args{tempFS, "notexist", false}, true},
+		{"Force remove", args{tempFS, "file.go", true}, true},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -38,22 +43,78 @@ func TestRmdir(t *testing.T) {
 		fs   afero.Fs
 		path string
 	}
+
+	// Create new in-memory FS with -
+	// One file, One empty directory, One directory with files
 	tempFS := afero.NewMemMapFs()
+	_, _ = tempFS.Create("file.go")
+	_ = tempFS.Mkdir("emptydir", 0755)
+	_ = tempFS.MkdirAll("first/second", 0755)
+	_, _ = tempFS.Create("first/second/third.go")
+
 	tests := []struct {
 		name    string
 		args    args
 		wantErr bool
 	}{
 		// Add test entries here
-		{"Remove example", args{tempFS, "../example"}, false},
-		// BUG: This works, but it should not, according to IsSafeToRemove
-		{"Should not remove", args{tempFS, "."}, false},
+		{"Remove empty directory", args{tempFS, "emptydir"}, false},
+		{"Remove file", args{tempFS, "file.go"}, false},
+		{"Remove directory recursively", args{tempFS, "first"}, false},
+		{"Try on non-existent file", args{tempFS, "notexist.go"}, false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if err := Rmdir(tt.args.fs, tt.args.path); (err != nil) != tt.wantErr {
 				t.Errorf("Rmdir() error = %v, wantErr %v", err, tt.wantErr)
 				test.ExpectedError(t, nil, err)
+			}
+		})
+	}
+}
+
+func TestCopyFromOS(t *testing.T) {
+	type args struct {
+		targetFs afero.Fs
+		src      string
+		dest     string
+		fileOnly bool
+	}
+
+	// Create blank in-memory FS
+	// One directory for source files, one for destination files
+	// One nested directory for source files being copied with structure
+	// One file in the root directory to copy into the destination
+	// One empty directory, one with no read permissions
+	tempFS := afero.NewMemMapFs()
+	_ = tempFS.Mkdir("src", 0755)
+	_, _ = tempFS.Create("src/srcfile1.go")
+	_ = tempFS.Mkdir("dest", 0755)
+	_ = tempFS.MkdirAll("nestdir/levelone", 0755)
+	_, _ = tempFS.Create("nestdir/levelone/srcfile2.go")
+	_, _ = tempFS.Create("srcfile3.go")
+	_ = tempFS.Mkdir("empty", 0755)
+	_ = tempFS.Mkdir("restricted", 0000)
+	_, _ = tempFS.Create("restricted/cantread.go")
+
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		// Add test cases here
+		{"Copy from src to dest dir", args{tempFS, "src", "dest", true}, false},
+		{"Copy with source structure", args{tempFS, "nestdir", "dest", false}, false},
+		{"Copy file from parent of dest", args{tempFS, "srcfile3.go", "dest", false}, false},
+		{"Destination directory does not exist", args{tempFS, "nestdir", "newdest", false}, false},
+		{"Source directory is empty", args{tempFS, "empty", "dest", false}, false},
+		{"Source file does not exist", args{tempFS, "nofile.go", "dest", true}, false},
+		{"Source directory has no read perms", args{tempFS, "restricted", "dest", false}, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := CopyFromOS(tt.args.targetFs, tt.args.src, tt.args.dest, tt.args.fileOnly); (err != nil) != tt.wantErr {
+				t.Errorf("CopyFromOS() error = %v, wantErr %v", err, tt.wantErr)
 			}
 		})
 	}
